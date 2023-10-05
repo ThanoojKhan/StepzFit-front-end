4af44f4993686fcb59e046b6ff7c4d516875a0aa
Thanooj <thanu1495@gmail.com>
2023-10-05 21:30:05 +0530

    google indexing jsonld

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.huvaq3m32tg"
+    "revision": "0.jsc7ttkrhrg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
